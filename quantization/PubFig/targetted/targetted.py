41cf5458d4d9a5f17f878013d5bde2ef98870f0b
Aahil Awatramani <44123487+AahilA@users.noreply.github.com>
2022-02-13 19:26:13 -0500

    Create targetted.py
    Comments added

--- quantization/PubFig/targetted/targetted.py
+++ quantization/PubFig/targetted/targetted.py
@@ -1,24 +1,23 @@
 import numpy as np
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
-os.environ['TF_DETERMINISTIC_OPS'] = '1'
+import json
+import keras_vggface
 import tensorflow as tf
 from tensorflow.python.framework.ops import enable_eager_execution
 enable_eager_execution()
 from tensorflow import keras
 import tensorflow_model_optimization as tfmot
-
-import json
-
-import time
-import keras_vggface
 from tensorflow.python.keras import backend
 from keras.engine import  Model
 from keras.layers import Flatten, Dense, Input
 from keras_vggface.vggface import VGGFace
 from tensorflow.keras import backend as K
 
+# Environment Variables
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
 
+#Helper functions
 def normalize(x):
     # utility function to normalize a tensor by its L2 norm
     return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
@@ -34,9 +33,10 @@ def preprocess_input_t(x):
         x = backend.bias_add(x, mean_tensor, data_format=backend.image_data_format())
     return x
 
-
+#Famous people subset to attack
 famous = ["Robert Downey Jr", "Nicolas Cage", "John Travolta", "Karl Rove", "Diane Sawyer", "Eva Mendes", "Anderson Cooper", "Gillian Anderson", "Jeri Ryan", "Tony Blair"]
 
+#Load Dataset
 test_x,test_y= np.load('../../../datasets/PubFig/test_x_1164.npy'),np.load('../../../datasets/PubFig/test_y_1164.npy')
 
 # Opening JSON file
@@ -46,6 +46,7 @@ f = open("'../../../datasets/PubFig/identities_decoder.json",)
 # a dictionary
 decode  = json.load(f)
 
+#Setting up models
 input = tf.keras.Input(shape=(224, 224, 3))
 vgg_model = VGGFace(include_top=False, input_tensor=input,model='resnet50')
 x = Flatten(name='flatten')(vgg_model.output)
@@ -63,31 +64,38 @@ model.trainable =False
 model.compile()
 q_model.compile()
 
+#Targetted face recognition attack
 def attack():
-    data=[]
+    
+    data=[] #Data will be stored here
 
     for i,celeb_image in enumerate(test_x):
+        #Only pick our subset of celebs to attack
         if decode[str(test_y[i])] not in famous:
             continue
-        winners = {}
+            
+        winners = {} #People we can attack
         image = celeb_image
         print("Real Label + " + decode[str(test_y[i])])
         orig_label = test_y[i]
         is_bad_image = False
         
+        # Go through all other possible celebs and try to make an attack
         for celeb in range(150):    
             
             if celeb == orig_label:
                 continue
             
             celebName = decode[str(celeb)]
-
+            
+            # hyper-parameters 
             grad_iterations = 20
             step = 1
             epsilon = 8
             A = 0
             c = 1
 
+            #Input images and real labels
             input_image = K.constant(image)
             orig_img = tf.identity(input_image)
             fp_logist = tf.identity(model.predict(preprocess_input_t(input_image)[None,...]))
@@ -97,7 +105,7 @@ def attack():
             quant_logist = tf.identity(q_model.predict(preprocess_input_t(input_image)[None,...]))
             quant_label =  np.argmax(quant_logist[0])
 
-
+            #If both the quantized and fp models disagree on a label we skip the image
             if orig_label != quant_label or orig_label != fp_label:
                 print("bad image!")
                 is_bad_image = True
@@ -105,7 +113,8 @@ def attack():
 
             A = 0
             for iters in range(grad_iterations):
-
+                
+                #Compute loss
                 with tf.GradientTape() as g:
                     g.watch(input_image)
                     loss1 = K.mean(model(preprocess_input_t(input_image)[None,...], training = False)[..., orig_label])
@@ -113,23 +122,24 @@ def attack():
                     loss3 = tf.keras.losses.categorical_crossentropy(tf.one_hot(celeb,150), q_model(preprocess_input_t(input_image)[None,...], training = False)[0])
                     final_loss = K.mean(loss1 - c*loss2 -  100*loss3)
 
-
+                # Compute attack
                 grads = normalize(g.gradient(final_loss, input_image))
                 adv_image = input_image + tf.sign(grads) * step
                 A = tf.clip_by_value(adv_image - orig_img, -epsilon, epsilon)
                 input_image = tf.clip_by_value(orig_img + A, 0, 255)
                 test_image = preprocess_input_t(input_image)[None,...]
 
-
+                
                 pred1, pred2= model.predict(test_image), q_model.predict(test_image)
                 label1, label2 = np.argmax(pred1[0]), np.argmax(pred2[0])
 
-
+                #If we successfully fool the quantized model but not the fp model we count it as a success
                 if label2 == celeb:
                     if label1 == orig_label:
                         # print("{} success with {}".format(decode[str(test_y[i])], celebName))
                         winners[celebName] = (float(pred2[0][celeb]), iters)
                         break
+        #Append new data                
         if not is_bad_image:
             s = decode[str(test_y[i])] + " " + str(i)
             data.append({s: winners})
